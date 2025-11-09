deb5978e8e822673b3f0f57a7508df1018e723de
achakraborty-ecw <achakraborty-ecw@cricut.com>
2025-11-09 15:47:28 +0530

    init

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('Hello, IIIT BGP!');
+});
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`BGP app is running on port ${PORT}`);
+});
